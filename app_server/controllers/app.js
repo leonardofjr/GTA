1d1a2cbfb33a7b7bcf43d48b2b5956442dee6168
leonardofjr <leonardo.f.jr@gmail.com>
2017-11-21 10:03:17 -0500

    Updated Facebook Login Functionality

--- app_server/controllers/app.js
+++ app_server/controllers/app.js
@@ -1,3 +1,6 @@
 module.exports.index = function(req, res, next) {
     res.render('index', { title: 'Express' });
 }
+module.exports.login = function(req, res, next) {
+    res.render('index', { title: 'Express' });
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,5 +4,6 @@ var ctrlMain = require('../controllers/app.js');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
+router.get('/login', ctrlMain.login);
 
 module.exports = router;
